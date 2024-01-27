5bc9d4bb223c97d33e5b4d26fe25556684b75fd1
Jeremy Liu <jliuu@umich.edu>
2024-01-27 15:12:36 -0500

    fixed small thing

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import LoginButton from "@/components/auth/login-btn";
 import UploadButton from "@/components/upload-image";
-import Icon from "@/app/favicon.ico";
+// import Icon from "@/app/favicon.ico";
 
 export default function Home() {
   return (
     <main className={"h-screen overscroll-none bg-blue-100"}>
       <div className={"flex"}>
         <div className={"mx-auto"}>
-          <Image src={Icon} alt={"hello"} width={150} height={150} className={"object-center"}/>
+          {/*<Image src={Icon} alt={"hello"} width={150} height={150} className={"object-center"}/>*/}
           <h1 className={"text-center mt-10 text-6xl roboto"}>
             Welcome to [name]
           </h1>
