5ee511d07f0276437c12d2a520f788a6c2debd48
Jeremy Liu <jliuu@umich.edu>
2024-01-27 14:09:42 -0500

    added basic auth

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import LoginButton from "@/components/auth/login-btn";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world
+      <LoginButton />
+    </div>
+  );
+}
