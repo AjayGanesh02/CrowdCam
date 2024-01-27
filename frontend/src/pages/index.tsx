e227a0e43cb8cfc19d897adb2c80c21ddc290c66
Jeremy Liu <jliuu@umich.edu>
2024-01-27 17:21:14 -0500

    made frontpage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,31 @@
+import Logo from "../../public/logo.svg";
 import Image from "next/image";
-import LoginButton from "@/components/auth/login-btn";
-import UploadButton from "@/components/upload-image";
-// import Icon from "@/app/favicon.ico";
 
 export default function Home() {
   return (
-    <main className={"h-screen overscroll-none bg-blue-100"}>
-      <div className={"flex"}>
-        <div className={"mx-auto"}>
-          {/*<Image src={Icon} alt={"hello"} width={150} height={150} className={"object-center"}/>*/}
-          <h1 className={"text-center mt-10 text-6xl roboto"}>
-            Welcome to [name]
-          </h1>
-          {/*<LoginButton/>*/}
-          {/*<UploadButton/>*/}
+    <main className={"h-screen overscroll-none bg-gradient-to-t from-black to-[#292F36]"}>
+      <div className={"flex items-center text-center px-10"}>
+        <div className={"mt-48 mx-auto"}>
+          <Image src={Logo} alt={"logo"} width={150} className={"mx-auto"}/>
+          <p
+            className={"text-center mt-8 text-5xl sm:text-6xl font-bold bg-gradient-to-r from-purple-800 to-blue-500 text-transparent bg-clip-text"}>
+            CROWDCAM
+          </p>
+          <p className={"text-2xl sm:text-3xl px-4 text-[#BFD7FF] mt-4 sm:mt-7 font-bold"}>
+            Find yourself amongst the crowds
+          </p>
+          <div>
+            <button
+              className={"text-white border w-80 py-4 border-[#292f36] rounded-xl bg-[#292F36] bg-opacity-40 mt-16 text-lg"}>
+              Find Yourself
+            </button>
+          </div>
+          <div>
+            <button
+              className={"text-white border w-80 py-4 border-[#292f36] rounded-xl bg-[#292F36] bg-opacity-40 text-lg mt-12"}>
+              Help Others
+            </button>
+          </div>
         </div>
       </div>
     </main>
