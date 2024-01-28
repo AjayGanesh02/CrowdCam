f8ae3b6af075fb2b845fd6582b20ec42ec8aa524
Jeremy Liu <jliuu@umich.edu>
2024-01-27 23:52:21 -0500

    refactored profile page and combined it with query

--- frontend/src/pages/query.tsx
+++ frontend/src/pages/query.tsx
@@ -1,47 +1,71 @@
-import { Carousel } from "@material-tailwind/react";
-import {useState} from "react";
+import { useProfileImage } from "@/contexts/ProfileContext";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ImageUpload from "@/components/upload-image";
 
 const QueryPage = () => {
-  const [queriedImages, setQueriedImages] = useState([
-    "https://crowdcamimages.s3.amazonaws.com/uploads/812c36650da0ab87b027f7407IMG_5179.jpg",
-    "https://crowdcamimages.s3.amazonaws.com/uploads/implicit_bias.jpg",
-    "https://crowdcamimages.s3.amazonaws.com/uploads/IMG_5174.jpg",
-    "https://crowdcamimages.s3.amazonaws.com/uploads/implicit_bias.jpg",
-    "https://crowdcamimages.s3.amazonaws.com/uploads/812c36650da0ab87b027f7407IMG_5179.jpg",
-  ]);
-  const [ active, setActive ] = useState(
-    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    );
 
-    return (
-      <main
-        className={"h-screen overscroll-none bg-gradient-to-t from-black to-[#292F36]"}
-      >
-        <div className={"flex items-center text-center px-10"}>
-          <div className="grid gap-4">
+  const [queryResults, setQueryResults] = useState<string[]>([]);
+  const [ active, setActive ] = useState("");
+
+  useEffect(() => {
+    if(queryResults.length > 0) {
+      setActive(queryResults[0]);
+    }
+  }, [queryResults]);
+
+  return (
+    <main
+      className={
+        "h-screen overscroll-none bg-gradient-to-t from-black to-[#292F36] text-center"
+      }
+    >
+      <div className={"text-3xl pt-8 px-8 text-[#BFD7FF] font-bold"}>
+        <p>Profile Page</p>
+      </div>
+      <div className={"mt-12"}>
+        {
+          queryResults.length == 0 ? (
             <div>
-              <img
-                className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
-                src={active}
-                alt=""
+              <p className={"text-white my-8"}>
+                We don&apos;t have an image for you. Please upload one!
+              </p>
+              <ImageUpload
+                search={true}
+                setter={(reply: any) => {
+                  setQueryResults(reply.matches)}
+                }
               />
             </div>
-            <div className="grid grid-cols-5 gap-4">
-              {queriedImages.map((imgelink, index) => (
-                <div key={index}>
+          )
+            :
+            <div className={"flex items-center text-center px-10"}>
+              <div className="grid gap-4">
+                <div>
                   <img
-                    onClick={() => setActive(imgelink)}
-                    src={imgelink}
-                    className="h-20 max-w-full cursor-pointer rounded-lg object-cover object-center"
-                    alt="gallery-image"
+                    className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
+                    src={active}
+                    alt=""
                   />
                 </div>
-              ))}
+                <div className="grid grid-cols-5 gap-4">
+                  {queryResults?.map((imgelink, index) => (
+                    <div key={index}>
+                      <img
+                        onClick={() => setActive(imgelink)}
+                        src={imgelink}
+                        className="h-20 max-w-full cursor-pointer rounded-lg object-cover object-center"
+                        alt="gallery-image"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </main>
-    )
-  }
+        }
+      </div>
+    </main>
+  );
+};
 
-      export default QueryPage
+export default QueryPage;
