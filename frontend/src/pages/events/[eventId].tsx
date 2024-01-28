ac6e1799d0ac07bcdaebcee426d1f3c5f4943c66
Jeremy Liu <jliuu@umich.edu>
2024-01-27 22:58:53 -0500

    small changes

--- frontend/src/pages/events/[eventId].tsx
+++ frontend/src/pages/events/[eventId].tsx
@@ -3,8 +3,14 @@ import ImageUploadButton from "@/components/upload-image";
 const EventUploadPage = () => {
   return (
     <main className={"h-screen overscroll-none bg-gradient-to-t from-black to-[#292F36]"}>
-      Upload image here
-      <ImageUploadButton />
+      <div className={"pt-8 px-4"}>
+        <div className={"text-3xl py-4 text-[#BFD7FF] font-bold text-center"}>
+          <p>
+            Upload Images Here
+          </p>
+        </div>
+        <ImageUploadButton/>
+      </div>
     </main>
   )
 }
