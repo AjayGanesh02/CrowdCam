6f9015bca4fc5176352f41de784fab564fdf9ad7
Jeremy Liu <jliuu@umich.edu>
2024-01-28 03:43:15 -0500

    fixed up build issues

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,9 @@
-import { SessionProvider } from "next-auth/react"
 import "./globals.css"
 export default function App({
                               Component,
                               pageProps: { session, ...pageProps },
                             }: any) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Component {...pageProps} />
   )
 }
