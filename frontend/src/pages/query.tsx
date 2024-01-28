885c7a400a694843c3dcce39afde969355d69b70
ashleychen738 <ashleychen738@gmail.com>
2024-01-27 23:37:20 -0500

    query page

--- frontend/src/pages/query.tsx
+++ frontend/src/pages/query.tsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
 import { Carousel } from "@material-tailwind/react";
+import {useState} from "react";
 
 const QueryPage = () => {
   const [queriedImages, setQueriedImages] = useState([
@@ -8,43 +8,40 @@ const QueryPage = () => {
     "https://crowdcamimages.s3.amazonaws.com/uploads/IMG_5174.jpg",
     "https://crowdcamimages.s3.amazonaws.com/uploads/implicit_bias.jpg",
     "https://crowdcamimages.s3.amazonaws.com/uploads/812c36650da0ab87b027f7407IMG_5179.jpg",
-  ])
+  ]);
+  const [ active, setActive ] = useState(
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    );
 
-  return (
-    <div className={"overscroll-none bg-gradient-to-t from-black to-[#292F36] text-center"}>
-      <Carousel className="rounded-xl">
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-      </Carousel>
-      <div className={"text-3xl pt-8 px-8 text-[#BFD7FF] font-bold"}>
-        <p>
-          Queried Images
-        </p>
-      </div>
-      <div className={"px-4"}>
-        {
-          queriedImages.map((image, index) => {
-            return (
-                <img src={image} className={"max-h-96 max-w-96 mx-auto rounded-md my-4"}/>
-            )
-          })
-        }
-      </div>
-    </div>
-  )
-}
+    return (
+      <main
+        className={"h-screen overscroll-none bg-gradient-to-t from-black to-[#292F36]"}
+      >
+        <div className={"flex items-center text-center px-10"}>
+          <div className="grid gap-4">
+            <div>
+              <img
+                className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
+                src={active}
+                alt=""
+              />
+            </div>
+            <div className="grid grid-cols-5 gap-4">
+              {queriedImages.map((imgelink, index) => (
+                <div key={index}>
+                  <img
+                    onClick={() => setActive(imgelink)}
+                    src={imgelink}
+                    className="h-20 max-w-full cursor-pointer rounded-lg object-cover object-center"
+                    alt="gallery-image"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
 
-export default QueryPage
+      export default QueryPage
